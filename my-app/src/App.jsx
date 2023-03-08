ddd135fc80e6b32a343fd5406670c295adadc19e
RokasSweets <sweetsound741@gmail.com>
2023-03-08 12:29:40 +0200

    catchup

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-4'>
+            Column
+        </div>
+        <div className='col-8'>
+            Column
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
